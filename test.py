dddf1f0605669e6a6f951cbda89409ec2e9e4a6a
tripod31 <tripod31@hotmail.com>
2016-02-12 20:14:18 +0900

    upd upd upd ok! upd

--- test.py
+++ test.py
@@ -1,6 +1,83 @@
 # -*- coding: utf_8 -*-
-#import uno
+import uno
+import unohelper
+from com.sun.star.awt import XActionListener    #@UnresolvedImport
 
+class MyActionListener( unohelper.Base, XActionListener ):
+    def __init__(self, dialog,callback):
+        self.dialog = dialog
+        self._callback = callback
+
+    def actionPerformed(self, actionEvent):
+        self._callback(self.dialog)
+
+#disp text of active cell in dialog
+def disp_str(arg):
+    #create dialog
+    if IS_EXECUTED_EXTERNAL:
+        ctx = XSCRIPTCONTEXT.getComponentContext()  #get context from XSCRIPTCONTEXT
+        dp = ctx.getServiceManager().createInstanceWithContext("com.sun.star.awt.DialogProvider", ctx)
+    else:
+        localctx = uno.getComponentContext()  #get local component context
+        dp = localctx.getServiceManager().createInstanceWithContext("com.sun.star.awt.DialogProvider", localctx)
+    
+    oDialog = dp.createDialog("vnd.sun.star.script:Standard.Dialog1?location=application")
+    
+    redisp(oDialog)
+
+    #set callback funcs
+    oDialog.getControl("button_close").addActionListener(MyActionListener(oDialog,end_dialog))
+    oDialog.getControl("button_ok").addActionListener(MyActionListener(oDialog,ok))
+    oDialog.getControl("button_redisp").addActionListener(MyActionListener(oDialog,redisp))
+    oDialog.getControl("button_up").addActionListener(MyActionListener(oDialog,up))
+    oDialog.getControl("button_down").addActionListener(MyActionListener(oDialog,down))
+    oDialog.getControl("button_close").addActionListener(MyActionListener(oDialog,end_dialog))
+    
+    down(oDialog)    
+    #display dailog
+    oDialog.execute()
+    
+#redisp text of active cell in dialog
+def redisp(oDialog):
+    oText =oDialog.getControl ("TextField1")
+    oCell = get_active_cell()
+    if oCell:
+        oText.Text = oCell.Text.String
+
+'''
+callback funcs
+'''
+
+#set text of dialog to active cell
+def set_str(oDialog):
+    oText =oDialog.getControl ("TextField1")
+    oCell = get_active_cell()
+    if oCell.Text.String!=oText.Text:
+        oCell.Text.String=oText.Text
+
+def ok(oDialog):
+    set_str(oDialog)
+    oDialog.endExecute()
+
+#↑
+def up(oDialog):
+    set_str(oDialog)
+    if activate_cell_offset(0,-1):
+        redisp(oDialog)
+
+#↓
+def down(oDialog):
+    set_str(oDialog)
+    if activate_cell_offset(0,1):
+        redisp(oDialog)
+
+def end_dialog(oDialog):
+    oDialog.endExecute()        
+
+ 
+'''
+common funcs
+'''
 def show_message(desktop, message):
     """shows message."""
     frame = desktop.getCurrentFrame()
@@ -11,21 +88,51 @@ def show_message(desktop, message):
     return msgbox.execute()
 
 
-def get_active_cell():
-    try:
-        doc = XSCRIPTCONTEXT.getDocument()
-        selected = doc.getCurrentSelection()
-        if selected.supportsService('com.sun.star.sheet.SheetCellRange'):
-            addr = selected.getRangeAddress()
-            
-            txt = 'Column: %s\nRow: %s' % (addr.EndColumn, addr.EndRow)
-            show_message(XSCRIPTCONTEXT.getDesktop(), txt)
-    except Exception as e:
-        print(str(e))
+'''
+:returns:    True:Success False:Fail
+'''
+def activate_cell_offset(argOffsetColumn,argOffsetRow):
+    oActiveCell = get_active_cell()
+    if oActiveCell is None:
+        return False
+    else:
+        new_cell = get_cell_offset(oActiveCell, argOffsetColumn, argOffsetRow)
+        if new_cell:
+            activate_cell(new_cell)
+        else:
+            return False
+        
+        return True
+    
+def get_cell_offset(argBaseRange,argOffsetColumn,argOffsetRow):
+    if argBaseRange.ImplementationName=="ScCellObj":
+        lngStartColumn = argBaseRange.CellAddress.Column + argOffsetColumn
+        lngStartRow = argBaseRange.CellAddress.Row + argOffsetRow
+        oSheet = argBaseRange.Spreadsheet
+        try:
+            oRange = oSheet.getCellByPosition(lngStartColumn, lngStartRow)
+        except:
+            oRange = None
+        return oRange
+    else:
+        return None
 
+def activate_cell(selection):
+    if selection.ImplementationName=='ScCellObj':
+        oController = XSCRIPTCONTEXT.getDocument().getCurrentController()
+        oController.setActiveSheet(selection.Spreadsheet)
+        oController.select(selection)
+    
+def get_active_cell():
+    doc = XSCRIPTCONTEXT.getDocument()
+    selection = doc.getCurrentSelection()
+    if selection.ImplementationName=='ScCellObj':
+        return selection
+    else:
+        return None        
    
 if __name__ == '__main__':
-    import uno 
+#    import uno
     localContext = uno.getComponentContext()
     resolver = localContext.ServiceManager.createInstanceWithContext("com.sun.star.bridge.UnoUrlResolver", localContext )
     ctx = resolver.resolve( "uno:socket,host=localhost,port=2002;urp;StarOffice.ComponentContext" )
@@ -35,5 +142,8 @@ if __name__ == '__main__':
     
     import unopy
     XSCRIPTCONTEXT = unopy.ScriptContext(ctx)
+    IS_EXECUTED_EXTERNAL = True
     
-    get_active_cell()
+    disp_str(None)
+else:
+    IS_EXECUTED_EXTERNAL = False
